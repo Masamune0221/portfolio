d50bde44c9cc9993edde5e0de1d64977c77a8396
Masamune0221 <dev.masamune02210@gmail.com>
2026-04-19 11:41:31 +0900

    2026/4/19 Masamune デプロイ_2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://Masamune0221.github.io",
-  base: "/portfolio",
+  base: "/portfolio/",
   vite: {
     plugins: [tailwindcss()],
   },
