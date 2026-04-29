f8569cee6898deb5bca211e7957a9120e590aecc
Masamune0221 <dev.masamune02210@gmail.com>
2026-04-29 17:35:28 +0900

    2026/4/29 Masamune GoogleAnalytics修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,21 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import partytown from "@astrojs/partytown";
+import { config } from "astro:schema";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://Masamune0221.github.io",
   base: "/portfolio/",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [partytown({
+    config:{
+      forward: ["dataLayer.push"]
+    }
+  })],
 });
